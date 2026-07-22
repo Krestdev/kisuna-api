a53cda9152bc5355f3e91904b0545faa66dc29ad
FNMALIC <fonkounixon@gmail.com>
2026-07-22 15:46:22 +0100

    feat: initialize application root module and configure authentication module with JWT strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    UsersModule,
     PassportModule,
     JwtModule.register({
       global: true,
